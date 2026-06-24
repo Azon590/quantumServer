8524db14001d0d6e7a0e5a5006dff8e1afc7b903
membae <membaphilip1@gmail.com>
2026-06-24 14:47:21 +0300

    port

--- app.py
+++ app.py
@@ -345,8 +345,8 @@ class WalletByID(Resource):
         return {"message": "Wallet deleted successfully"}, 200
 api.add_resource(WalletByID, "/wallet/<int:id>")
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     app.run(debug=True, host="127.0.0.1", port=5000)
